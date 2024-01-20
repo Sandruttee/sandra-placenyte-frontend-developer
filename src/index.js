941c71705d4a21e00b272d4fafdbc3ce4376a792
Sandruttee <sandra.placenyte@gmail.com>
2024-01-20 12:31:11 +0100

    uopdated with a typewriter plugin

---
+++ src/index.js
@@ -0,0 +1,10 @@
+new Typewriter("#about", {
+  strings: [
+    `I'm a detail-oriented, front-end developer. I love developing
+            responsive front-end websites and applications using HTML, CSS,
+            JavaScript, React.js, Bootstrap, API and other tools. I'm currently
+            seeking for new opportunities in front-end development.`,
+  ],
+  autoStart: true,
+  delay: 10,
+});
