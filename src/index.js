09ed5ce84420c7ebb9e5e6f6c314073ae40f4e0e
Sandruttee <sandra.placenyte@gmail.com>
2024-02-12 18:53:05 +0100

    Bug fix
    The typewriter was deleting the text after it's finished

--- src/index.js
+++ src/index.js
@@ -1,10 +1,9 @@
 new Typewriter("#about", {
-  strings: [
-    `I'm a detail-oriented, front-end developer. I love developing
+  strings: `I'm a detail-oriented, front-end developer. I love developing
             responsive front-end websites and applications using HTML, CSS,
             JavaScript, React.js, Bootstrap, API and other tools. I'm currently
             seeking for new opportunities in front-end development.`,
-  ],
+
   autoStart: true,
   delay: 10,
 });
